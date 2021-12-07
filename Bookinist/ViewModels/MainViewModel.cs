f2896edac57309c1a26f148f77b0c25602603756
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-07 16:37:25 +0300

    Пробная загрузка

--- Bookinist/ViewModels/MainViewModel.cs
+++ Bookinist/ViewModels/MainViewModel.cs
@@ -1,19 +1,36 @@
-﻿using MathCore.ViewModels;
+﻿using Bookinist.DAL.Entities;
+using Bookinist.Interfaces;
+
+using MathCore.ViewModels;
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Bookinist.ViewModels
 {
   public  class MainViewModel: ViewModel
     {
+        private readonly IRepository<Book> _bookRepository;
+
+
+      
+        
+        
+
+        #region Конструктор
+        public MainViewModel(IRepository<Book> BookRepository)
+        {
+            _bookRepository = BookRepository;
+            var books = BookRepository.Items.Take(10).ToArray();
+        }
+        #endregion
 
 
         #region  string Title Заголовок
         ///<summary> Заголовок
         private string _Title ="Главное окно";
-        ///<summary> Заголовок
         public string Title
         {
             get => _Title;
