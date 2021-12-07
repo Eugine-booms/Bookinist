19230f3498d473bc25c11a99980c30ba1a5a762a
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-07 16:02:37 +0300

    терфейсы Интерфейс IEntity и Irepositary

---
+++ Bookinist.Interfaces/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookinist.Interfaces
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
