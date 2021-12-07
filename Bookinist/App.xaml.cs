744c3d84f90d12f8d014ec00cbd16b3e66a41255
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-07 11:31:47 +0300

    Host  его настройка

--- Bookinist/App.xaml.cs
+++ Bookinist/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Bookinist.Services;
+using Bookinist.ViewModels;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +19,24 @@ namespace Bookinist
     /// </summary>
     public partial class App : Application
     {
+        private static IHost __Host;
+        public static IHost Host => __Host
+            ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services) =>
+            services.RegisterServices().RegisterViewModels();
+        public static IServiceProvider Services => Host.Services;
+        protected async override void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            await host.StartAsync().ConfigureAwait(false);
+            base.OnStartup(e);
+        }
+        protected async override void OnExit(ExitEventArgs e)
+        {
+            var host = Host;
+            base.OnExit(e);
+            await host.StopAsync().ConfigureAwait(false);
+            host = null;
+        }
     }
 }
