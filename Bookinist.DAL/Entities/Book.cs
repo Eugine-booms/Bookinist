0f6a66f93ee961c26107f6d2c035608a54db58bc
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-07 14:58:19 +0300

    Создание модели БД

---
+++ Bookinist.DAL/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿using Bookinist.DAL.Entities.Base;
+
+using System;
+using System.Text;
+
+namespace Bookinist.DAL.Entities
+{
+    public class Book : NamedEntity
+    {
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Bookinist.DAL/Entities/Deal.cs
@@ -0,0 +1,17 @@
+﻿using Bookinist.DAL.Entities.Base;
+
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bookinist.DAL.Entities
+{
+    public class Deal : Entity
+    {
+        public virtual Book  Book{get; set;}
+        public virtual Seller Seller { get; set; }
+        public  virtual Buyer Buyer { get; set; }
+        [Column(TypeName="decimal(18,2)")]
+        public virtual decimal Price { get; set; }
+
+    }
+}
