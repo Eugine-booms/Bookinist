a0d1cfed96574e041d7f592a4a11a333cfffca2c
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-07 16:36:34 +0300

    Создание отдельных репозитариев да сущностей с зависимостями

---
+++ Bookinist.DAL/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Bookinist.DAL.Context;
+using Bookinist.DAL.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+using System.Linq;
+
+namespace Bookinist.DAL
+{
+    class CategoryRepository : DbRepository<Category>
+    {
+        public CategoryRepository(BookinistDb db) : base(db)
+        {
+        }
+
+        public override IQueryable<Category> Items => base.Items.Include(item => item.Books);
+    }
---
+++ Bookinist.DAL/DealRepository.cs
@@ -0,0 +1,21 @@
+﻿using Bookinist.DAL.Context;
+using Bookinist.DAL.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+using System.Linq;
+
+namespace Bookinist.DAL
+{
+    class DealRepository : DbRepository<Deal>
+    {
+        public DealRepository(BookinistDb db) : base(db)
+        {
+        }
+
+        public override IQueryable<Deal> Items => 
+            base.Items
+            .Include(item => item.Book)
+            .Include(item => item.Seller)
+            .Include(item => item.Buyer);
+}
