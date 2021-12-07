31811ebac92752b9a55a266371d9743e7f23dbb3
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-07 11:31:47 +0300

    Host  его настройка

---
+++ Bookinist/Services/UserDialog.cs
@@ -0,0 +1,14 @@
+﻿using Bookinist.Services.Interfaces;
+
+using MathCore.WPF.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookinist.Services
+{
+  internal   class UserDialog : IUserDialog
+    {
+    }
+}
---
+++ Bookinist/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MathCore.ViewModels;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookinist.ViewModels
+{
+  public  class MainViewModel: ViewModel
+    {
+
+
+        #region  string Title Заголовок
+        ///<summary> Заголовок
+        private string _Title ="Главное окно";
+        ///<summary> Заголовок
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value, nameof(Title));
+        }
+        #endregion
+
+    }
+}
